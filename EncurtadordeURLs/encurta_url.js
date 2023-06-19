6677c25b6b061516d3c706c58d50d2a0a3aaddb8
Your Name <lklukas@outlook.com>
2023-06-19 16:02:02 -0300

    fix(remoção do servidor nodejs)

--- EncurtadordeURLs/encurta_url.js
+++ EncurtadordeURLs/encurta_url.js
@@ -1,16 +1,11 @@
-var entradaurl = "https://www.youtube.com/watch?v=8BAl6z0lXdw"; // Defina o valor da entradaurl
+var entradaurl = "https://www.youtube.com/watch?v=8BAl6z0lXdw";
+const url = 'https://is.gd/create.php?format=simple&url=';
 
-fetch(`http://localhost:3000/api/${entradaurl}`)
-  .then(response => response.text()) // Alteração aqui para obter o corpo da resposta como texto
+fetch(url + encodeURIComponent(entradaurl))
+  .then(response => response.text())
   .then(data => {
-    console.log('Resposta do servidor Node:', data); // Exibir o corpo da resposta no console
-    try {
-      const json = JSON.parse(data); // Tentar fazer o parsing do JSON
-      console.log('Resposta do servidor Node (JSON):', json);
-    } catch (error) {
-      console.error('Erro na análise do JSON:', error);
-    }
+    console.log('Resultado da requisição:', data);
   })
   .catch(error => {
-    console.error('Erro na chamada do servidor:', error);
+    console.error('Erro na requisição:', error);
   });
