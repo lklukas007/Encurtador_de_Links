73a4a753ca32ebd4069ae9c66b5d507e18c8642e
Your Name <lklukas@outlook.com>
2023-06-21 15:21:36 -0300

    fix(script.js): req realizada pelo js separado srv

--- EncurtadordeURLs/script.js
+++ EncurtadordeURLs/script.js
@@ -1,14 +1,15 @@
+var entradaurl = 'https://www.google.com.br/';
+const url = 'http://localhost:3000/api/';
 
-  var entradaurl = 'https://www.google.com.br/url?sa=i&url=https%3A%2F%2Fwww.vakinha.com.br%2Fvaquinha%2Ffrutaria-do-james&psig=AOvVaw13V8MZd1fzeUyh75RgBuZS&ust=1687455359940000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCLixhJrz1P8CFQAAAAAdAAAAABAJ'
-
-  const url = 'http://localhost:3000/api/:';
-
-  try{
-  fetch(url + entradaurl);
-  response => response.text();
-  console.log(response);
-  }
-  catch(error)
-  {console.error('Erro na requisição:', error);
-  };
-
+try {
+  fetch(url + entradaurl)
+    .then(response => response.text())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Erro na requisição:', error);
+    });
+} catch (error) {
+  console.error('Erro na requisição:', error);
+}
